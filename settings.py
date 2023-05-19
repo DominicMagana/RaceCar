12d175cd9acc6a232cdac94302f78aff2a625093
DominicMagana <127146887+DominicMagana@users.noreply.github.com>
2023-05-19 09:31:33 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ from sprites import *
 
 pg.init()
 
+BLACK = 0,0,0
 WIDTH = 400
 HEIGHT = 400
 WHITE = (225,255,225)
@@ -18,7 +19,9 @@ music = pg.mixer.music.load('Fluffing-a-Duck.mp3')
 pg.mixer.music.play(-1)
 collect_sound = pg.mixer.Sound('Collect.mp3')
 x, y = 200, 200
+# defining these variables to determine how far each movement is
 delta_x, delta_y = 10, 0
+# making the food go on random places of the screen
 food_x, food_y = random.randrange(0, WIDTH)//10*10, random.randrange(0,HEIGHT)//10*10 
 body_list = [(x,y)]
 clock = pg.time.Clock()
